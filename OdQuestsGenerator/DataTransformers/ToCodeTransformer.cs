04e6580e92148acc423d5968708294e3cf191870
Artem Khoschenko <multiteemer@gmail.com>
2017-08-27 12:23:56 +1000

    Remove useless method

--- OdQuestsGenerator/DataTransformers/ToCodeTransformer.cs
+++ OdQuestsGenerator/DataTransformers/ToCodeTransformer.cs
@@ -5,15 +5,6 @@ namespace OdQuestsGenerator.DataTransformers
 {
 	static class ToCodeTransformer
 	{
-		public static string Transform(State state, Quest quest)
-		{
-			var t = new StateTaskTemplate {
-				StateEnumName = quest.Name + "QuestState",
-				StateName = state.Name,
-			};
-			return t.TransformText();
-		}
-
 		public static string Transform(Quest quest)
 		{
 			var t = new QuestClassTemplate {
