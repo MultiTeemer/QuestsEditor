359618f4b8367352c100ca0f9c10f00f7464b15e
monoklid <multiteemer@gmail.com>
2017-09-14 16:11:54 +1000

    [Code] Add saving

--- OdQuestsGenerator/Forms/QuestsViewerStuff/Code.cs
+++ OdQuestsGenerator/Forms/QuestsViewerStuff/Code.cs
@@ -1,5 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Formatting;
+using Microsoft.CodeAnalysis.Formatting;
 using OdQuestsGenerator.Data;
 using OdQuestsGenerator.Utils;
 
@@ -15,7 +19,20 @@ namespace OdQuestsGenerator.Forms.QuestsViewerStuff
 	class CodeBulk
 	{
 		public readonly CodeBulkType Type;
-		public readonly SyntaxTree Tree;
+
+		private SyntaxTree tree;
+
+		public SyntaxTree Tree
+		{
+			get => tree;
+			set
+			{
+				if (value != tree) {
+					tree = value;
+					WasModified = true;
+				}
+			}
+		}
 	
 		public bool WasModified { get; set; }
 
@@ -32,6 +49,7 @@ namespace OdQuestsGenerator.Forms.QuestsViewerStuff
 	{
 		private readonly List<CodeBulk> codeBulks = new List<CodeBulk>();
 		private readonly Dictionary<string, CodeBulk> fileToCodeBulk = new Dictionary<string, CodeBulk>();
+		private readonly Dictionary<CodeBulk, string> codeBulkToFile = new Dictionary<CodeBulk, string>();
 		private readonly Dictionary<Quest, CodeBulk> questToCodeBulk = new Dictionary<Quest, CodeBulk>();
 		private readonly Dictionary<Sector, CodeBulk> sectorToCodeBulk = new Dictionary<Sector, CodeBulk>();
 
@@ -64,12 +82,32 @@ namespace OdQuestsGenerator.Forms.QuestsViewerStuff
 			return sectorToCodeBulk[sector];
 		}
 
+		public void Save()
+		{
+			var modifiedCodeBulks = codeBulks.Where(cb => cb.WasModified);
+			foreach (var codeBulk in modifiedCodeBulks) {
+				WriteCodeToFile(codeBulk.Tree, codeBulkToFile[codeBulk]);
+				codeBulk.WasModified = false;
+			}
+		}
+
+		private void WriteCodeToFile(SyntaxTree tree, string filePath)
+		{
+			using (var writer = new StreamWriter(File.OpenWrite(filePath))) {
+				var workspace = new AdhocWorkspace();
+				var options = workspace.Options.WithChangedOption(CSharpFormattingOptions.NewLinesForBracesInTypes, true);
+				var res = Formatter.Format(tree.GetRoot(), workspace, options);
+				writer.WriteLine(res);
+			}
+		}
+
 		private CodeBulk ReadFromFileAndCache(string path, CodeBulkType type)
 		{
 			var tree = FileSystem.ReadCodeFromFile(path);
 			var bulk = new CodeBulk(type, tree);
 			codeBulks.Add(bulk);
 			fileToCodeBulk.Add(path, bulk);
+			codeBulkToFile.Add(bulk, path);
 
 			return bulk;
 		}
